2dc67870b3fe9c3db63193c737bd81c999650e7b
Laura G <elizabethgzzp@gmail.com>
2020-07-17 01:40:36 +0200

    Creación de rama primeros cambios

--- fichero2.java
+++ fichero2.java
@@ -1,10 +1,13 @@
-public static boolean esPrimo(int numero){
-  int contador = 2;
-  boolean primo=true;
-  while ((primo) && (contador!=numero)){
-    if (numero % contador == 0)
-      primo = false;
-    contador++;
-  }
-  return primo;
+public class EsPrimo {
+
+	public static boolean esPrimo(int numero){
+	  int contador = 2;
+	  boolean primo=true;
+	  while ((primo) && (contador!=numero)){
+	    if (numero % contador == 0)
+	      primo = false;
+	    contador++;
+	  }
+	  return primo;
+	}
 }
