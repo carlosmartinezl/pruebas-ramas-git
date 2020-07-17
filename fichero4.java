c66b4bd0484935f19842abf5db9a336df8ee13ff
Pablo G <camartinezl1@gmail.com>
2020-07-17 02:06:36 +0200

    Creación de rama bugfix
    Esta rama se ha creado para forzar el conflicto

--- fichero4.java
+++ fichero4.java
@@ -15,7 +15,21 @@ public class NumeroMayorApp {
                 System.out.println("El número "+num1+" es mayor que el número "+num2);
             }
         }else{
-            System.out.println("El número "+num2+" es mayor que el número "+num1);
+            double discriminante=Math.pow(b, 2)-(4*a*c);
+  
+	        //Mensaje de traza
+	        System.out.println(">>"+discriminante);
+	  
+	        if (discriminante>0){
+	            //Tambien se puede hacer por parte si lo ves más facil
+	            //recuerda que debes tener en cuenta la prioridad de elementos
+	            double x1=((b*(-1))+Math.sqrt(discriminante))/(2*a);
+	            double x2=((b*(-1))-Math.sqrt(discriminante))/(2*a);
+	  
+	            System.out.println("El valor de x1 es "+x1+" y el valor de x2 es "+x2);
+	        }else{
+	            System.out.println("El discriminante es negativo");
+	        }
         }
     }
 }
